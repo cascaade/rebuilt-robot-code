5788c0b7767725ccd05a544e381c298c3a414cfc
Owen <189063419+cascaade@users.noreply.github.com>
2026-03-21 14:44:41 -0400

    Move auto to its own file

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,31 +5,22 @@
 
 package frc.robot;
 
-import java.util.function.Supplier;
-
-import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.LoggedNetworkBoolean;
 
 import choreo.auto.AutoFactory;
-import choreo.auto.AutoRoutine;
-import choreo.auto.AutoTrajectory;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StringPublisher;
 import edu.wpi.first.networktables.StringSubscriber;
 import edu.wpi.first.wpilibj.Preferences;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.Constants.ShooterConstants;
 import frc.robot.Constants.VisionConstants;
+import frc.robot.autos.AutoBrain;
 import frc.robot.subsystems.climb.Climb;
 import frc.robot.subsystems.climb.ClimbIO;
 import frc.robot.subsystems.climb.ClimbIOSpark;
@@ -65,32 +56,13 @@ public class RobotContainer {
 
     private final LoggedNetworkBoolean runAutoBoolean = new LoggedNetworkBoolean("Auto/RunAuto", true);
 
-    private final AutoFactory autoFactory;
-    private final StringPublisher autoPathPublisher;
-    private final StringSubscriber autoPathSubscriber;
-    private final SendableChooser<String> autoMode = new SendableChooser<>();
+    private final AutoBrain autoBrain;
 
     public RobotContainer() {
-        autoMode.setDefaultOption("Auto 1", "Auto1");
-        autoMode.addOption("Auto 2", "Auto2");
-        autoMode.addOption("Auto 3", "Auto3");
-        SmartDashboard.putData("Auto", autoMode);
-
-        var topic = NetworkTableInstance.getDefault()
-            .getStringTopic("/SmartDashboard/Auto Path");
-        autoPathPublisher = topic.publish();
-        autoPathPublisher.set("1,2,5,6"); // default shown in textbox
-        autoPathSubscriber = topic.subscribe("1,2,5,6");
-
         Preferences.removeAll();
 
         driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
         auxController = new CommandXboxController(OperatorConstants.kAuxControllerPort);
-
-        // var topic = NetworkTableInstance.getDefault().getStringTopic("/SmartDashboard/Auto Path");
-
-        // StringPublisher autoPathPublisher = topic.publish();
-        // StringSubscriber autoPathSubscriber = topic.subscribe("Auto1__2_5");;
 
         switch(Constants.currentMode) {
             case REAL:
@@ -186,13 +158,7 @@ public class RobotContainer {
                 break;
         }
 
-        autoFactory = new AutoFactory(
-            swerve::getPose,
-            swerve::resetOdometry,
-            swerve::followTrajectory,
-            true,
-            swerve
-            );
+        autoBrain = new AutoBrain(swerve, shooter, intake);
 
         configureBindings();
     }
@@ -236,161 +202,11 @@ public class RobotContainer {
         auxController.rightBumper().onTrue(intake.toggleRollerFlag());
     }
 
-    public AutoRoutine buildAuto() {
-        String autoNum = autoMode.getSelected();
-        String pathName = autoPathSubscriber.get();
-        
-        String[] points = pathName.split(",");
-        
-        AutoRoutine auto = autoFactory.newRoutine("auto");
-
-        if (pathName.isEmpty()) {
-            // CHANGE SHOOTER LOGIC
-            AutoTrajectory path = auto.trajectory(autoNum+"__1_Preload");
-            auto.active().onTrue(Commands.sequence(
-                path.resetOdometry(),
-                path.cmd(),
-                shooter.toggleRunShooter(),
-                new WaitCommand(5),
-                shooter.toggleRunIndex(),
-                new WaitCommand(5),
-                shooter.toggleRunIndex(),
-                shooter.toggleRunShooter()
-            ));
-            return auto;
-        }
-
-        if (points.length < 2) {
-            System.out.println("Not enough points");
-            return auto;
-        }
-
-        String[] pathNames = new String[points.length-1];
-        AutoTrajectory[] paths = new AutoTrajectory[points.length-1];
-
-        for (int i=0; i<points.length-1; i++) {
-            pathNames[i] = autoNum+"__"+points[i]+"_"+points[i+1];
-            paths[i] = auto.trajectory(pathNames[i]);
-        }
-
-        // Debug only
-        for(String s : pathNames) {
-            System.out.println(s);
-        }
-
-        auto.active().onTrue(Commands.sequence(
-            paths[0].resetOdometry(),
-            shooter.toggleRunShooter(),
-            paths[0].cmd()
-        ));
-
-        for (int i = 0; i < paths.length - 1; i++) {
-            String EP = points[i+1];
-            String pathN = pathNames[i];
-            if (shouldShootAfter(EP)) {
-                paths[i].done().onTrue(Commands.sequence(
-                    shooter.toggleRunShooter(),
-                    new WaitCommand(5),
-                    shooter.toggleRunIndex(),
-                    new WaitCommand(5),
-                    // shooter.toggleRunIndex(),
-                    // shooter.toggleRunShooter(),
-                    paths[i+1].cmd()
-                ));
-            } 
-            else if (shouldIntakeDuring(pathN)) {
-                paths[i].active().onTrue(intake.toggleRollerFlag());
-                paths[i].done().onTrue(Commands.sequence(
-                    intake.toggleRollerFlag(),
-                    paths[i+1].cmd()
-                ));
-            }
-            else {
-                paths[i].done().onTrue(paths[i+1].cmd());
-            }
-        }
-
-        String lastEP = points[points.length-1];
-        AutoTrajectory lastPath = paths[paths.length-1];
-        String lastPathName = pathNames[pathNames.length-1];
-
-        if (shouldShootAfter(lastEP)) {
-            // NEW SHOOTER LOGIC
-            paths[paths.length - 1].done().onTrue(Commands.sequence(
-                swerve.runToggleToXPosition(),
-                swerve.runToggleAimHub(),
-                new WaitCommand(1),
-                shooter.toggleRunIndex(),
-                new WaitCommand(2),
-                swerve.runToggleToXPosition(),
-                shooter.toggleRunIndex(),
-                shooter.toggleRunShooter()
-            ));
-        }
-        else if (shouldIntakeDuring(lastPathName)) {
-            lastPath.active().onTrue(intake.toggleRollerFlag());
-            lastPath.done().onTrue(intake.toggleRollerFlag());
-        }
-
-        return auto;
-        
-        // pathNames = new String[pathName.length()-1];
-        // AutoTrajectory[] paths = new AutoTrajectory[pathName.length()-1];
-        // for (int i=0; i<pathName.length()-1; i++) {
-        //     AutoTrajectory path = auto.trajectory(autoNum+"__"+pathName.substring(i, i+1)+"_"+pathName.substring(i+1, i+2));
-        //     pathNames[i] = autoNum+"__"+pathName.substring(i, i+1)+"_"+pathName.substring(i+1, i+2);
-        //     paths[i] = path;
-        // }
-        
-        // for(String s : pathNames) {
-        //     System.out.println(s);
-        // }
-        
-        // auto.active().onTrue(Commands.sequence(
-        //     paths[0].resetOdometry(),
-        //     paths[0].cmd()
-        // ));
-        // for (int i=1; i<pathNames.length-1; i++) {
-        //     String EP = pathNames[i].substring(pathNames[i].length()-2);
-        //     if (EP.equals("4a") || EP.equals("4b") || EP.equals("_4") || EP.equals("_6") 
-        //     || EP.equals("6a") || EP.equals("6b")) {
-        //         paths[i].done().onTrue(
-        //         Commands.sequence(
-        //             shooter.toggleRunShooter(),
-        //             new WaitCommand(5),
-        //             shooter.toggleRunIndex(),
-        //             new WaitCommand(5),
-        //             shooter.toggleRunIndex(),
-        //             shooter.toggleRunShooter(),
-        //             paths[i+1].cmd()
-        //         )
-        //     );
-        //     }
-        //     else {
-        //         paths[i].done().onTrue(paths[i+1].cmd());
-        //     }
-        // }
-        
-        // return auto;
-    }
-
-    private boolean shouldShootAfter(String EP) {
-        return EP.equals("4a") || EP.equals("4b") || EP.equals("4") || EP.equals("6") 
-            || EP.equals("6a") || EP.equals("6b");
-    }
-
-    private boolean shouldIntakeDuring(String path) {
-        return path.equals("Auto2__2_5") || path.equals("Auto2__5_6a") || path.equals("Auto2__2_3") ||
-        path.equals("Auto3__5_6") || path.equals("Auto1__2_3") || path.equals("Auto2__2_4a") ||
-        path.equals("Auto2__5_2") || path.equals("Auto1__2_5") || path.equals("Auto3__5_2") ||
-        path.equals("Auto1__2_4");
-    }
-
     public void testPeriodic() {
         swerve.periodic();
     }
 
     public Command getAutonomousCommand() {
-            return buildAuto().cmd();
+        return autoBrain.buildAuto().cmd();
     }
 }
---
+++ src/main/java/frc/robot/autos/AutoBrain.java
@@ -0,0 +1,201 @@
+package frc.robot.autos;
+
+import choreo.auto.AutoFactory;
+import choreo.auto.AutoRoutine;
+import choreo.auto.AutoTrajectory;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StringPublisher;
+import edu.wpi.first.networktables.StringSubscriber;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.subsystems.swerve.SwerveDrive;
+
+public class AutoBrain {
+    private final AutoFactory autoFactory;
+    private final StringPublisher autoPathPublisher;
+    private final StringSubscriber autoPathSubscriber;
+    private final SendableChooser<String> autoMode = new SendableChooser<>();
+
+    private final SwerveDrive swerveSubsystem;
+    private final Shooter shooterSubsystem;
+    private final Intake intakeSubsystem;
+
+    public AutoBrain(SwerveDrive swerveSubsystem, Shooter shooterSubsystem, Intake intakeSubsystem) {
+        this.swerveSubsystem = swerveSubsystem;
+        this.shooterSubsystem = shooterSubsystem;
+        this.intakeSubsystem = intakeSubsystem;
+
+        autoMode.setDefaultOption("Auto 1", "Auto1");
+        autoMode.addOption("Auto 2", "Auto2");
+        autoMode.addOption("Auto 3", "Auto3");
+        SmartDashboard.putData("Auto", autoMode);
+
+        var topic = NetworkTableInstance.getDefault()
+            .getStringTopic("/SmartDashboard/Auto Path");
+        autoPathPublisher = topic.publish();
+        autoPathPublisher.set("1,2,5,6"); // default shown in textbox
+        autoPathSubscriber = topic.subscribe("1,2,5,6");
+
+        autoFactory = new AutoFactory(
+            swerveSubsystem::getPose,
+            swerveSubsystem::resetOdometry,
+            swerveSubsystem::followTrajectory,
+            true,
+            swerveSubsystem
+        );
+    }
+
+    public AutoRoutine buildAuto() {
+        String autoNum = autoMode.getSelected();
+        String pathName = autoPathSubscriber.get().trim();
+
+        String[] points = pathName.split(",");
+
+        AutoRoutine auto = autoFactory.newRoutine("auto");
+
+        if (pathName.isEmpty()) {
+            // CHANGE SHOOTER LOGIC
+            AutoTrajectory path = auto.trajectory(autoNum+"__1_Preload");
+            auto.active().onTrue(Commands.sequence(
+                path.resetOdometry(),
+                path.cmd(),
+                shooterSubsystem.toggleRunShooter(),
+                new WaitCommand(5),
+                shooterSubsystem.toggleRunIndex(),
+                new WaitCommand(5),
+                shooterSubsystem.toggleRunIndex(),
+                shooterSubsystem.toggleRunShooter()
+            ));
+            return auto;
+        }
+
+        if (points.length < 2) {
+            System.out.println("Not enough points");
+            return auto;
+        }
+
+        String[] pathNames = new String[points.length-1];
+        AutoTrajectory[] paths = new AutoTrajectory[points.length-1];
+
+        for (int i=0; i<points.length-1; i++) {
+            pathNames[i] = autoNum+"__"+points[i]+"_"+points[i+1];
+            paths[i] = auto.trajectory(pathNames[i]);
+        }
+
+        // Debug only
+        for(String s : pathNames) {
+            System.out.println(s);
+        }
+
+        auto.active().onTrue(Commands.sequence(
+            paths[0].resetOdometry(),
+            shooterSubsystem.toggleRunShooter(),
+            paths[0].cmd()
+        ));
+
+        for (int i = 0; i < paths.length - 1; i++) {
+            String EP = points[i+1];
+            String pathN = pathNames[i];
+            if (shouldShootAfter(EP)) {
+                paths[i].done().onTrue(Commands.sequence(
+                    shooterSubsystem.toggleRunShooter(),
+                    new WaitCommand(5),
+                    shooterSubsystem.toggleRunIndex(),
+                    new WaitCommand(5),
+                    // shooterSubsystem.toggleRunIndex(),
+                    // shooterSubsystem.toggleRunShooter(),
+                    paths[i+1].cmd()
+                ));
+            }
+            else if (shouldIntakeDuring(pathN)) {
+                paths[i].active().onTrue(intakeSubsystem.toggleRollerFlag());
+                paths[i].done().onTrue(Commands.sequence(
+                    intakeSubsystem.toggleRollerFlag(),
+                    paths[i+1].cmd()
+                ));
+            }
+            else {
+                paths[i].done().onTrue(paths[i+1].cmd());
+            }
+        }
+
+        String lastEP = points[points.length-1];
+        AutoTrajectory lastPath = paths[paths.length-1];
+        String lastPathName = pathNames[pathNames.length-1];
+
+        if (shouldShootAfter(lastEP)) {
+            // NEW SHOOTER LOGIC
+            paths[paths.length - 1].done().onTrue(Commands.sequence(
+                swerveSubsystem.runToggleToXPosition(),
+                swerveSubsystem.runToggleAimHub(),
+                new WaitCommand(1),
+                shooterSubsystem.toggleRunIndex(),
+                new WaitCommand(2),
+                swerveSubsystem.runToggleToXPosition(),
+                shooterSubsystem.toggleRunIndex(),
+                shooterSubsystem.toggleRunShooter()
+            ));
+        }
+        else if (shouldIntakeDuring(lastPathName)) {
+            lastPath.active().onTrue(intakeSubsystem.toggleRollerFlag());
+            lastPath.done().onTrue(intakeSubsystem.toggleRollerFlag());
+        }
+
+        return auto;
+
+        // pathNames = new String[pathName.length()-1];
+        // AutoTrajectory[] paths = new AutoTrajectory[pathName.length()-1];
+        // for (int i=0; i<pathName.length()-1; i++) {
+        //     AutoTrajectory path = auto.trajectory(autoNum+"__"+pathName.substring(i, i+1)+"_"+pathName.substring(i+1, i+2));
+        //     pathNames[i] = autoNum+"__"+pathName.substring(i, i+1)+"_"+pathName.substring(i+1, i+2);
+        //     paths[i] = path;
+        // }
+
+        // for(String s : pathNames) {
+        //     System.out.println(s);
+        // }
+
+        // auto.active().onTrue(Commands.sequence(
+        //     paths[0].resetOdometry(),
+        //     paths[0].cmd()
+        // ));
+        // for (int i=1; i<pathNames.length-1; i++) {
+        //     String EP = pathNames[i].substring(pathNames[i].length()-2);
+        //     if (EP.equals("4a") || EP.equals("4b") || EP.equals("_4") || EP.equals("_6")
+        //     || EP.equals("6a") || EP.equals("6b")) {
+        //         paths[i].done().onTrue(
+        //         Commands.sequence(
+        //             shooterSubsystem.toggleRunShooter(),
+        //             new WaitCommand(5),
+        //             shooter.toggleRunIndex(),
+        //             new WaitCommand(5),
+        //             shooter.toggleRunIndex(),
+        //             shooter.toggleRunShooter(),
+        //             paths[i+1].cmd()
+        //         )
+        //     );
+        //     }
+        //     else {
+        //         paths[i].done().onTrue(paths[i+1].cmd());
+        //     }
+        // }
+
+        // return auto;
+    }
+
+    private boolean shouldShootAfter(String EP) {
+        return EP.equals("4a") || EP.equals("4b") || EP.equals("4") || EP.equals("6")
+            || EP.equals("6a") || EP.equals("6b");
+    }
+
+    private boolean shouldIntakeDuring(String path) {
+        return path.equals("Auto2__2_5") || path.equals("Auto2__5_6a") || path.equals("Auto2__2_3") ||
+            path.equals("Auto3__5_6") || path.equals("Auto1__2_3") || path.equals("Auto2__2_4a") ||
+            path.equals("Auto2__5_2") || path.equals("Auto1__2_5") || path.equals("Auto3__5_2") ||
+            path.equals("Auto1__2_4");
+    }
+}
